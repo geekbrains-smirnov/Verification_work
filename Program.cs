c73c9366614146d4712022aaad557207bbe9cefc
Artem Smirnov <expertvideo.as@gmail.com>
2022-09-07 14:50:08 +0300

    решение при помощи оператора foreach

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+
+Console.Write("Введите массив через пробел состоящий из строк: ");
+string array = Console.ReadLine();
+string[] arr = array.Split();
+foreach(string word in arr)
+{
+    if(word.Length<=3) Console.WriteLine(word);
+    else Console.WriteLine("Длина строк больше 3 символов");
+}
