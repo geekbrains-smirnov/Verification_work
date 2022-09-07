538da2ada788915dcda8badc460fe7851502836c
Artem Smirnov <expertvideo.as@gmail.com>
2022-09-07 16:04:19 +0300

    вывел из метода PrintStrings заполнение массива с клавиатуры в отдельный метод AmountStrings

--- Program.cs
+++ Program.cs
@@ -1,47 +1,27 @@
 ﻿
-
-
-
-// Console.Write("Введите размер массива: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// string[] str = new string[m];
-// for(int i = 0; i<m; i++)
-// {
-//     Console.Write("Введите строку: ", i+1);
-//     str[m] = Console.ReadLine();
-// }
-// Console.WriteLine("Вы ввели следующие строки:");
-//             for (int i = 0; i < m; i++)
-//             {
-//                 Console.WriteLine(str[i]);
-//             }
-//             Console.Write("Нажмите [Enter] для выхода...");
-//             Console.ReadLine();
-// Console.Write("Введите массив через пробел состоящий из строк: ");
-// str[m] = Console.ReadLine();
-// // foreach(string word in arr)
-// // {
-// //     if(word.Length<=3) Console.WriteLine(word);
-// //     else Console.WriteLine("Длина строк больше 3 символов");
-// // }
-// Console.WriteLine(str);
-
-
 Console.Write("Введите количество строк, которые хотите ввести: ");
 int n = Convert.ToInt32(Console.ReadLine()); 
-string[] strs = new string[n]; 
-void PrintStrings(string[] strs)
+string[] strs = new string[n];
+string[] str = new string[n];
+void AmountStrings(string[]strs)
 {
-    for (int i = 0; i < n; i++)
+for (int i = 0; i < n; i++)
     {
         Console.Write("Введите строку: ");
         strs[i] = Console.ReadLine();
     }
-    Console.WriteLine("Массив из строк: ");
+}
+
+void PrintStrings(string[] strs)
+{
+    
     for (int i = 0; i < n; i++)
         {
             Console.Write($"{strs[i]} ");
         }
+        Console.WriteLine();
 }        
-PrintStrings(strs);
+
+
+
 
