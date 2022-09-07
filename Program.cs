613c3feb0ee9a68edcd3b6fbf8ddd3e09dc0d082
Artem Smirnov <expertvideo.as@gmail.com>
2022-09-07 16:30:57 +0300

    отформатирован код

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿
 Console.Write("Введите количество строк, которые хотите ввести: ");
-int n = Convert.ToInt32(Console.ReadLine()); 
+int n = Convert.ToInt32(Console.ReadLine());
 string[] strs = new string[n];
 string[] str = new string[n];
-void AmountStrings(string[]strs)
+void AmountStrings(string[] strs)
 {
-for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         Console.Write("Введите строку: ");
         strs[i] = Console.ReadLine();
@@ -14,29 +14,29 @@ for (int i = 0; i < n; i++)
 
 void PrintStrings(string[] strs)
 {
-    
-    for (int i = 0; i < n; i++)
-        {
-            Console.Write($"{strs[i]} ");
-        }
-        Console.WriteLine();
-}        
 
-void StringsNew(string[] strs, string[]str)
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"{strs[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void StringsNew(string[] strs, string[] str)
 {
     int count = 0;
-    for(int i = 0; i<strs.Length; i++)
+    for (int i = 0; i < strs.Length; i++)
     {
-        if(strs[i].Length<=3)
+        if (strs[i].Length <= 3)
         {
             str[count] = strs[i];
             count++;
         }
-        
+
     }
 }
 AmountStrings(strs);
-StringsNew(strs,str);
+StringsNew(strs, str);
 Console.WriteLine();
 Console.WriteLine("Массив из строк с символами меньше или равное 3: ");
 PrintStrings(str);
